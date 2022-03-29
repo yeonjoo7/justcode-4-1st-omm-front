3e07aca62b726ac34fbd1f73e86ce17347d50509
shw779 <shw779@naver.com>
2022-03-29 16:05:59 +0900

    [ADD]초기설정 재설정

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/main/Main';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
