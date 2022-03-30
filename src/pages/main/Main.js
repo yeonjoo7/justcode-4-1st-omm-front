92c500187c4a84f8b8043659b1de7f99c6cf3a81
yeonjoo7 <jenny.likes.breeze@gmail.com>
2022-03-30 20:06:13 +0900

    [FIX] body 내용이 헤더 위로 올라오는 문제 수정

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 
 function Main() {
   return (
     <div>
       <Header />
+      Main
+      <Footer />
     </div>
   );
 }
