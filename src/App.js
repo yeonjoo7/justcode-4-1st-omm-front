5095a5efd70c957cbba34b8d6f11c1fbca4dbf2c
0teklee <leetekwoo@gmail.com>
2022-04-04 13:23:19 +0900

    [ADD] App.js에 master-detail 라우터 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import Main from './pages/main/Main';
 import Login from './pages/login/Login';
 import MasterList from './pages/master/MasterList';
 import ThemaCategoryList from './pages/category/ThemaCategoryList';
-<<<<<<< HEAD
-import MasterDetail from './pages/master-detail/MasterDetail';
-=======
 import UserSignUp from './pages/user-signup/UserSignup';
-// import MasterSignup from './pages/master-signup/MasterSignup';
->>>>>>> f2324b5a7036dcbca63e121e91457b2743a457fb
+import MasterSignup from './pages/master-signup/MasterSignUp';
+import MasterDetail from './pages/master-detail/MasterDetail';
 
 import './styles/reset.scss';
 import './styles/common.scss';
@@ -21,12 +18,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/:lesson_name" element={<ThemaCategoryList />} />
         <Route path="/master/list" element={<MasterList />} />
-<<<<<<< HEAD
-        <Route path="/profile/users/:id" element={<MasterDetail />} />
-=======
         <Route path="/sign-up" element={<UserSignUp />} />
         {/* <Route path="/pro/welcome" element={<MasterSignup />} /> */}
->>>>>>> f2324b5a7036dcbca63e121e91457b2743a457fb
       </Routes>
     </BrowserRouter>
   );
