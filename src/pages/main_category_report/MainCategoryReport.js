2496816df8e53c023d18ad18685b305927a5a246
shw779 <shw779@naver.com>
2022-04-05 10:39:09 +0900

    [FIX]warining 제거

--- src/pages/main_category_report/MainCategoryReport.js
+++ src/pages/main_category_report/MainCategoryReport.js
@@ -38,35 +38,38 @@ function MainCategoryReport() {
           </div>
           <div className={styles.calc_num}>
             <table>
-              <tr>
-                <td>19,181</td>
-                <td>24,865</td>
-                <td className={styles.table_end}>12,333</td>
-              </tr>
-              <tr>
-                <td>활동고수</td>
-                <td>누적 요청서</td>
-                <td className={styles.table_end}>리뷰 수</td>
-              </tr>
+              <tbody>
+                <tr>
+                  <td>19,181</td>
+                  <td>24,865</td>
+                  <td className={styles.table_end}>12,333</td>
+                </tr>
+                <tr>
+                  <td>활동고수</td>
+                  <td>누적 요청서</td>
+                  <td className={styles.table_end}>리뷰 수</td>
+                </tr>
+              </tbody>
             </table>
           </div>
           <div className={styles.wrap}>
             <ReportForm category={category} image={image} question={question} />
             <div className={styles.text_container}>
+              <div className={styles.strong_div}>
+                <strong>숨고는 어떤 곳인가요?</strong>
+              </div>
+              <br />
               <p className={styles.text_wrap}>
-                <div className={styles.strong_div}>
-                  <strong>숨고는 어떤 곳인가요?</strong>
-                </div>
-                <br />
                 서비스가 필요한 고객과 서비스를 제공하는 숨은 고수를 쉽고 빠르게
                 연결해드리는 전문가 매칭 서비스입니다. 1분 내외의 요청서를
                 작성하면, 여러 고수님들이 맞춤형 견적을 보내드려요. 맘에 쏙 드는
                 고수의 맞춤형 서비스를 받아보세요.
               </p>
+
+              <div className={styles.strong_div}>
+                <strong>방송댄스 레슨 고수만을 모았다!</strong>
+              </div>
               <p>
-                <div className={styles.strong_div}>
-                  <strong>방송댄스 레슨 고수만을 모았다!</strong>
-                </div>
                 <br /> 방송 댄스에 관심이 있는데 시작이 어려웠나요? 퇴근 후 취미
                 생활, 원데이 클래스 등 방송 댄스를 배워보고 싶다면 요청서를
                 작성해보세요. 선생님 프로필, 커리큘럼, 비용 비교하고 결정할 수
