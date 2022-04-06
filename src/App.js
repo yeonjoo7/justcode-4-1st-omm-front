5bf273abd34b2872f8fce37f1d694403ce574902
0teklee <leetekwoo@gmail.com>
2022-04-06 16:51:35 +0900

    [FIX] 고수 신청서 기능 구현 중

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Main from './pages/main/Main';
 import Login from './pages/login/Login';
 import MasterList from './pages/master/MasterList';
 import ThemaCategoryList from './pages/category/ThemaCategoryList';
-import UserSignUp from './pages/user-signup/UserSignup';
+// import UserSignUp from './pages/user-signup/UserSignup';
 import MasterSignUp from './pages/master-signup/MasterSignUp';
 import MasterDetail from './pages/master-detail/MasterDetail';
+import MasterSignUpNext from './pages/master-signup/MasterSignUpNext';
 
 import './styles/reset.scss';
 import './styles/common.scss';
+import MasterRequestForm from './components/master-detail/MasterRequestForm';
 
 function App() {
   return (
@@ -18,9 +20,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/:lesson_name" element={<ThemaCategoryList />} />
         <Route path="/master/list" element={<MasterList />} />
-        <Route path="/sign-up" element={<UserSignUp />} />
+        {/* <Route path="/sign-up" element={<UserSignUp />} /> */}
         <Route path="/profile/users/:id" element={<MasterDetail />} />
-        <Route path="/" element={<UserSignUp />} />
+        <Route path="/pro" element={<MasterSignUp />} />
+        <Route path="/pro/:id" element={<MasterSignUpNext />} />
+        <Route path="/formtest" element={<MasterRequestForm />} />
       </Routes>
     </BrowserRouter>
   );
