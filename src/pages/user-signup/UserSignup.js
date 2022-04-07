6e931a3605804a4c79f4a82036f6eba957a33b22
yeonjoo7 <jenny.likes.breeze@gmail.com>
2022-04-07 17:13:47 +0900

    signup 경로 수정

--- src/pages/user-signup/UserSignup.js
+++ src/pages/user-signup/UserSignup.js
@@ -1,13 +1,13 @@
 import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './UserSignUp.module.scss';
-import Header from 'src/components/header/Header';
+import Header from '../../components/header/Header';
 
 function UserSignUp() {
   // api로 보내기
 
   const sendUserSignUp = () => {
-    fetch('users/signup', {
+    fetch('http://localhost:8000/users/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
